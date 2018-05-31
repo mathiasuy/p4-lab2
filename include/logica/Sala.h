e113d10a17d9a8e979a166823616e32e5510e27b
Mathias Daniel Battistella Suarez <mathias.battistella@fing.edu.uy>
2018-05-31 13:48:40 -0300

    bool agregarFuncion(Funcion* funcion);         bool quitarFuncion(Funcion* funcion);         vector<Funcion*> listarFunciones();

--- include/logica/Sala.h
+++ include/logica/Sala.h
@@ -1,6 +1,9 @@
 #ifndef SALA_H
 #define SALA_H
 #include <string>
+#include <vector>
+#include "../datatypes/Funcion.h"
+
 using namespace std;
 
 class Sala
@@ -8,11 +11,15 @@ class Sala
     private:
         int id;
         int capacidad;
+        vector<Funcion*> funciones;
 
     public:
         int getID();
         int getCapacidad();
         void setCapacidad(int id);
+        bool agregarFuncion(Funcion* funcion);
+        bool quitarFuncion(Funcion* funcion);
+        vector<Funcion*> listarFunciones();
 
         bool isEqual(Sala* sala);
         string toString();
