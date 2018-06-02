30731d1edcbc0dd9865f7b4c0ef5d4a3faa6d534
Gonzalo R <gonxarc@hotmail.com>
2018-06-02 20:32:45 +0000

    altafuncion2

--- include/logica/Sistema.h
+++ include/logica/Sistema.h
@@ -11,7 +11,7 @@ class Sistema : public ISistema
         bool altaCine(int id, string direccion, vector<DtSala> salas);
 
         /*  PELICULA */
-        bbool altaFuncion(int asientos, float precioEntrada, DtFecha fecha, Sala* sala, Pelicula* pelicula);
+        bool altaFuncion(int asientos, float precioEntrada, DtFecha fecha, Sala* sala, Pelicula* pelicula);
         bool getPuntajePelicula(string nickName, DtPelicula pelicula);
         bool puntuarPelicula(string nickName, DtPelicula pelicula);
         bool eliminarPelicula(string titulo);
