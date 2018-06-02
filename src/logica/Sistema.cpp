e4bb5a735e9fce3f78c6890de4636ea9d35e8e48
Gonzalo R <gonxarc@hotmail.com>
2018-06-02 20:41:23 +0000

    altafuncion3

--- src/logica/Sistema.cpp
+++ src/logica/Sistema.cpp
@@ -2,7 +2,13 @@
 #include "include/logica/Cine.h"
 #include "include/manejadores/ManejadorCines.h"
 #include "include/manejadores/ManejadorUsuarios.h"
+#include "include/manejadores/ManejadorFunciones.h"
+#include "include/manejadores/ManejadorPeliculas.h"
+
 ManejadorCines *cines = ManejadorCines::getInstancia();
+ManejadorCines *funciones = Manejadorfunciones::getInstancia();
+ManejadorCines *usuarios = ManejadorUsuarios::getInstancia();
+ManejadorCines *peliculas = ManejadorPeliculas::getInstancia();
 
 
         Sistema* Sistema::instancia = NULL;
@@ -24,7 +30,8 @@ ManejadorCines *cines = ManejadorCines::getInstancia();
 
         /*  PELICULA */
         bool altaFuncion(int asientos, float precioEntrada, DtFecha fecha, Sala* sala, Pelicula* pelicula){
-            Funcion fnueva = new Funcion(int asientos, float precioEntrada, DtFecha fecha, Sala* sala , Pelicula *pelicula);
+            Funcion fnueva = new Funcion(asientos, precioEntrada, fecha, sala, pelicula);
+            funciones->add(fnueva);
             return true;
         };
 
