10182bbf718a7d0a1f6b78cff826f66eae180921
Nicolas Fernando Zorrozua Quintans <nicolas.zorrozua@fing.edu.uy>
2018-06-02 21:36:07 -0300

    Update Usuario.h

--- include/logica/Usuario.h
+++ include/logica/Usuario.h
@@ -22,8 +22,9 @@ class Usuario
 
         void setImagen(string imagen);
         void setPassword(string password);
+        bool tieneReservaFuncion(int id);
+        bool eliminarReservaConFuncion(int idFuncion);
 
-        
         bool agregarReservaDebito(int cantAsientos, Funcion* funcion, float montoTotal, string  banco);
         bool agregarReservaCredito(int cantAsientos, Funcion* funcion, float montoTotal, float descuento);
         bool quitarReserva(int id);
