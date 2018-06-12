3789cfa66318d0f4980807aca7e969fff1508214
Emilio Matias Costa Meneses <emilio.costa@fing.edu.uy>
2018-06-12 17:13:50 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -326,7 +326,41 @@ int main()
 
                         }; //END CASO DE USO
                         break;
-            case 6 : if (tipoUsuario==2){
+            case 6 : if (tipoUsuario==1){  //CASO DE USO COMENTAR PELICULA
+                                                                            string nomPelicula, come; //DECLARACION DE VARIABLES
+                                                                            int respuesta, esRespuestaDeID;
+                                                                            bool hecho;
+
+                                                                            ListaDt<string, DtPelicula> peliculas = interface->listarPeliculas();
+                                                                            cout << peliculas.toString();
+                                                                            cout << "Ingrese el nombre de la película que desea comentar: ";
+                                                                            std::cin.ignore();
+                                                                            std::getline(cin,nomPelicula);
+                                                                            do{
+                                                                           ListaDt<int,DtComentario> comentarios = interface->listarComentarios(nomPelicula);
+                                                                           cout << comentarios.toString();
+                                                                            cout << "Que acción desea realizar?   1- Comentar       2- Responder comentario     3- Salir";
+                                                                            cin >>respuesta;
+                                                                                    if  (respuesta == 1) {
+                                                                                                 cout << "Ingrese su comentario:";
+                                                                                                 std::cin.ignore();
+                                                                                                 std::getline(cin,come);
+                                                                                                 hecho = interface->comentarPelicula(nickName, nomPelicula, come);
+                                                                                                 cout << "Comentario ingresado correctamente !";
+                                                                                    }
+                                                                                    else if (respuesta == 2) {
+                                                                                        cout << "Ingrese el ID de el comentario que desea responder:";
+                                                                                        cin >> esRespuestaDeID;
+                                                                                        cout >> "Ingrese su comentario: ";
+                                                                                        std::cin.ignore();
+                                                                                        std::getline(cin,come);
+                                                                                        hecho = interface->comentarPelicula(nickName, nomPelicula, come, esRespuestaDeID);
+                                                                                    }
+                                                                            }
+                                                                            while (respuesta != 3);
+
+
+
 
                         };
                         break;
